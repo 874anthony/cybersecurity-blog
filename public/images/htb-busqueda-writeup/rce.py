2e59c86b6c1001e1a3ea1e88b50251a97333b4a7
Anthony Acosta M <anthony082008@hotmail.es>
2023-06-17 15:50:42 -0500

    Finished busqueda htb machine writeup

--- public/images/htb-busqueda-writeup/rce.py
+++
@@ -1,9 +0,0 @@
-class Engine:
-    class Bing:
-        @staticmethod
-        def search(query, copy_url=False):
-            return "https://bing.com/search?q=" + query
-
-url = eval(f"Engine.Bing.search('',__import__('os').system('echo Hello'))#', copy_url=True)")
-
-print(url)
